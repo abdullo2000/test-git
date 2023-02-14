7fd725362a356a9fcf4e0c91cc5203f1767af79e
abdullo2000 <goldabdullo@gmail.com>
2023-02-14 19:55:45 +0500

    second-commit

---
+++ script/main.js
@@ -0,0 +1,44 @@
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+// const alert = (message, type) => {
+//   const wrapper = document.createElement('div')
+//   wrapper.innerHTML = [
+//     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+//     `   <div>${message}</div>`,
+//     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//     '</div>'
+//   ].join('')
+
+//   alertPlaceholder.append(wrapper)
+// }
+
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', () => {
+//         ('Nice, you triggered this alert message!', 'success')
+//   })
+// }
+const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
+})
+
+
+const send = document.getElementById('send')
+
+send.addEventListener('click', () => {
+    const User__name = document.getElementById('User__name').value
+    const Parol = document.getElementById('Parol').value
+})
